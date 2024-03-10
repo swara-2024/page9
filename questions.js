b75fef4a28ed8f9be5845fa80a4aabdb70f85b49
swara-2024 <162478520+swara-2024@users.noreply.github.com>
2024-03-10 19:20:22 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "बारिश में हम किसका इस्तमाल करते हैं ?",
+    answer: "छत्री",
+    options: [
+      "छतड़ी ",
+      "छत्री",
+      "छतली",
+      "छती "
+    ]
+  },
+    {
+    numb: 2,
+    question: "आसमान में उड़ने वाले कौन हैं ?",
+    answer: "पक्षी",
+    options: [
+      "पक्ति",
+      "पकशी",
+      "पशि",
+      "पक्षी"
+    ]
+  },
+    {
+    numb: 3,
+    question: "आसमान में क्या चमकते हैं ?",
+    answer: "तारे",
+    options: [
+      "तारे",
+      "ताले",
+      "ताड़े",
+      "टाले"
+    ]
+  },
+    {
+    numb: 4,
+    question: "तोह बोलो खाना किससे खाओगे ?",
+    answer: "चम्मच",
+    options: [
+      "तम्मच",
+      "चलमच",
+      "चमच",
+      "चम्मच"
+    ]
+  },
+    {
+    numb: 5,
+    question: "फल किसपर लगते हैं ?",
+    answer: "वृक्ष",
+    options: [
+      "वृक्ष",
+      "वुक्स",
+      "वुक्ष",
+      "वुश"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
